763237e09917d514e276cbbfc26ad11a8fa05c0d
MikeTheFyke <mikefyke@hotmail.com>
2024-03-06 21:25:22 -0500

    Added nav hamburger button and mobile nav

---
+++ Components/Nav.tsx
@@ -0,0 +1,29 @@
+import { Bars3Icon } from "@heroicons/react/20/solid";
+import React from "react";
+
+interface Props {
+	openNav: () => void;
+}
+
+const Nav = ({ openNav }: Props) => {
+	return (
+		<div className="w-[100%] top-0 h-[12vh] bg-[#141c27] shadow-md">
+			<div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+				<h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
+					MIKE
+					<span className="text-red-500">FYKE</span>
+				</h1>
+				<div className="nav-link">HOME</div>
+				<div className="nav-link">SKILLS</div>
+				<div className="nav-link">PROJECTS</div>
+				<div className="nav-link">EXPERIENCE</div>
+				<div className="nav-link">CONTACT</div>
+				<div onClick={openNav}>
+					<Bars3Icon className="w-[2rem] md:hidden h-[2rem] cursor-pointer text-red-500" />
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import Nav from "@/Components/Nav";
+import MobileNav from "@/Components/MobileNav";
 
 const Homepage = () => {
-	return <div className="text-9xl text-red-900">home</div>;
+	const [nav, setNav] = useState(false);
+	const openNav = () => setNav(true);
+	const closeNav = () => setNav(false);
+
+	return (
+		<div className="overflow-x-hidden">
+			<div>
+				<MobileNav nav={nav} closeNav={closeNav} />
+				<Nav openNav={openNav} />
+			</div>
+		</div>
+	);
 };
 
 export default Homepage;
