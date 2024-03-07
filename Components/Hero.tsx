4f8a593e13d079f7c7f62929b0c739e2155d8b27
MikeTheFyke <mikefyke@hotmail.com>
2024-03-06 22:10:54 -0500

    Added text animation

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Particle from "./Particle";
+import TextEffect from "./TextEffect";
 
 const Hero = () => {
 	return (
 		<div className="h-[88vh]  bg-[#253348]">
 			<Particle />
+			<div className="w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center">
+				<h1 className="text-[35px] md:text-[50px] text-white font=bold">
+					Hey Hey! <span className="text-red-500">It's me Mike!</span>
+				</h1>
+				<TextEffect />
+			</div>
 		</div>
 	);
 };
