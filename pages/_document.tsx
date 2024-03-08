a5caaf07cc275823b69c7bb8f28ef9afe8baf7be
MikeTheFyke <mikefyke@hotmail.com>
2024-03-07 20:33:41 -0500

    Updated tab label

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
 	return (
 		<Html lang="en" className="overflow-x-hidden">
 			<Head />
+			<title>Mike Fykes Portfolio</title>
 			<body className="font-primary">
 				<Main />
 				<NextScript />
