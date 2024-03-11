b3ad72bcdab37fa972a07215a20e42657cc25d2f
MikeTheFyke <mikefyke@hotmail.com>
2024-03-11 14:14:46 -0400

    Added resume download

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
 					</p>
 					<div className="mt-[2rem] flex-col space-y-6 sm:space-y-o sm:flex sm:flex-row items-center sm:space-x-6">
 						<button className="px-[2rem] hover:bg-red-500 transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase bg-[#55e6a5] text-black flex items-center space-x-2">
-							<a download={"/MikeFyke-Resume.pdf"}>Download CV</a>
+							<a href="/MikeFyke-Resume.pdf">Download CV</a>
 							<ArrowDownTrayIcon className="w-[1.6rem] h[1.7rem] text-black" />
 						</button>
 					</div>
