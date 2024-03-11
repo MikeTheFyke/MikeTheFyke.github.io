58c089b052bb940a15b70fdbac10c2497e71878a
MikeTheFyke <mikefyke@hotmail.com>
2024-03-11 17:21:37 -0400

    Adding styles to project descriptions

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -11,36 +11,36 @@ const Projects = () => {
 				<div data-aos="fade-up">
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/BetterBytes-Main.jpeg" alt="portfolio" layout="fill" className="object-contain" />
-						<h1>Better Bytes</h1>
-						<h1>ReactJs</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">Better Bytes</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">ReactJs</h1>
 					</div>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="300">
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/OwlSite-Main.png" alt="portfolio" layout="fill" className="object-contain" />
-						<h1>Owl Site</h1>
-						<h1>Vue, GSAP, Express, MongoDB</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">Owl Site</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">Vue, GSAP, Express, MongoDB</h1>
 					</div>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="600">
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/ShrinkALink-Main.png" alt="portfolio" layout="fill" className="object-contain" />
-						<h1>Shrink-A-Link</h1>
-						<h1>NodeJs, Express, EJS, JQuery, Bootstrap</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">Shrink-A-Link</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">NodeJs, Express, EJS, JQuery, Bootstrap</h1>
 					</div>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="900">
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/ChatterBox-Main.png" alt="portfolio" layout="fill" className="object-contain" />
-						<h1>Chatter Box</h1>
-						<h1>NodeJs, React, React-dom, Express, Webpack</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">Chatter Box</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">NodeJs, React, React-dom, Express, Webpack</h1>
 					</div>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="1200">
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/Tweeter-Main.png" alt="portfolio" layout="fill" className="object-contain" />
-						<h1>Tweeter</h1>
-						<h1>HTML, CSS, JQuery, Express, MongoDB</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">Tweeter</h1>
+						<h1 className="text-[19px] text-slate-300 w-[80%]">HTML, CSS, JQuery, Express, MongoDB</h1>
 					</div>
 				</div>
 			</div>
