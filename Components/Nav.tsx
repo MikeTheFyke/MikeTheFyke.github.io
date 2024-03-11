878fc2b4794cb347b91891e7ffaee1f54eb3a9aa
MikeTheFyke <mikefyke@hotmail.com>
2024-03-11 10:42:49 -0400

    Added hero scroll to"

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -9,10 +9,12 @@ const Nav = ({ openNav }: Props) => {
 	return (
 		<div className="w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md">
 			<div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
-				<h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
-					MIKE
-					<span className="text-red-500">FYKE</span>
-				</h1>
+				<a href="#Hero">
+					<h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
+						MIKE
+						<span className="text-red-500">FYKE</span>
+					</h1>
+				</a>
 				<a className="nav-link" href="#Skills">
 					SKILLS
 				</a>
