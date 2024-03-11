e4a66a732138a9ace1082503a42991afac3b6d8f
MikeTheFyke <mikefyke@hotmail.com>
2024-03-11 10:27:12 -0400

    Added scroll smooth

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html lang="en" className="overflow-x-hidden">
+		<Html lang="en" className="overflow-x-hidden scroll-smooth">
 			<Head />
 			<title>Mike Fykes Portfolio</title>
 			<body className="font-primary">
