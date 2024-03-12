8b542c73b9abccb4703258226d42492ef600af77
MikeTheFyke <mikefyke@hotmail.com>
2024-03-12 09:43:43 -0400

    Added horizontal margin to skills

--- Components/SkillsLanguage.tsx
+++ Components/SkillsLanguage.tsx
@@ -8,7 +8,7 @@ interface Props {
 const SkillsLanguage = ({ skills, title }: Props) => {
 	return (
 		<div>
-			<div className="relative mb-[3rem] w-[80%] text-center">
+			<div className="relative mb-[3rem] w-[80%] text-center mx-2">
 				<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-white text-[20px] font-bold uppercase">{title}</h1>
 				<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-slate-300 text-[20px]">{skills}</h1>
 			</div>
