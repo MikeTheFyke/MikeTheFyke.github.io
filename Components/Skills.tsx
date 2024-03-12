1e42580c59272038bb87f15e865c945e2eabc8e4
MikeTheFyke <mikefyke@hotmail.com>
2024-03-12 09:24:17 -0400

    Updating skills sections

--- Components/Skills.tsx
+++ Components/Skills.tsx
@@ -11,13 +11,13 @@ const Skills = () => {
 			<div className="w-[80%] mx-auto pt-[4rem] md:pt-[8rem] grid grid-cols-1 md:grid-cols-2 gap-[2rem] items-center">
 				<div>
 					<SkillsItem title="Lighthouse Labs" year="2019" description="Fullstack Web Development bootcamp." />
-					<SkillsLanguage skills="NodeJs, HTML, CSS, Javascript, JQuery, Express, Cheerio, Axios, React, Ruby on Rails, SQL, PostgresSQL, MongoDB, Git, Knex" />
 				</div>
 				<div>
 					<SkillsItem title="Sheridan College" year="2007 - 2009" description="Visual & creative Arts (Advanced Diploma)" />
-					<SkillsLanguage skills="Traditional Drawing, Painting, Sculpting, 2D Design, 3D Design, Typography, Web Design, Graphic Design, Photography, Print Making" />
 				</div>
 			</div>
+			<SkillsLanguage skills="NodeJs, HTML, CSS, Javascript, JQuery, Express, Cheerio, Axios, React, Ruby on Rails, SQL, PostgresSQL, MongoDB, Git, Knex" />
+			<SkillsLanguage skills="Traditional Drawing, Painting, Sculpting, 2D Design, 3D Design, Typography, Web Design, Graphic Design, Photography, Print Making" />
 		</div>
 	);
 };
