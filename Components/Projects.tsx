f1385ce97699fd07e8689956ec37b9fba2b7dd64
MikeTheFyke <mikefyke@hotmail.com>
2024-03-11 20:44:34 -0400

    Updating app title styles

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -9,35 +9,37 @@ const Projects = () => {
 			</h1>
 			<div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
 				<div data-aos="fade-up">
-					<h1 className="text-[19px] text-slate-300 w-[80%]">Better Bytes</h1>
+					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">Better Bytes</h1>
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/BetterBytes-Main.jpeg" alt="portfolio" layout="fill" className="object-contain" />
 					</div>
 					<h1 className="text-[19px] text-slate-300 w-[80%]">ReactJs</h1>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="300">
-					<h1 className="text-[19px] text-slate-300 w-[80%]">Owl Site</h1>
+					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">Owl Site</h1>
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/OwlSite-Main.png" alt="portfolio" layout="fill" className="object-contain" />
 					</div>
 					<h1 className="text-[19px] text-slate-300 w-[80%]">Vue, GSAP, Express, MongoDB</h1>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="600">
-					<h1 className="text-[19px] text-slate-300 w-[80%]">Shrink-A-Link</h1>
+					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">
+						Shrink-A-Link
+					</h1>
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/ShrinkALink-Main.png" alt="portfolio" layout="fill" className="object-contain" />
 					</div>
 					<h1 className="text-[19px] text-slate-300 w-[80%]">NodeJs, Express, EJS, JQuery, Bootstrap</h1>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="900">
-					<h1 className="text-[19px] text-slate-300 w-[80%]">Chatter Box</h1>
+					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">Chatter Box</h1>
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/ChatterBox-Main.png" alt="portfolio" layout="fill" className="object-contain" />
 					</div>
 					<h1 className="text-[19px] text-slate-300 w-[80%]">NodeJs, React, React-dom, Express, Webpack</h1>
 				</div>
 				<div data-aos="fade-up" data-aos-delay="1200">
-					<h1 className="text-[19px] text-slate-300 w-[80%]">Tweeter</h1>
+					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white">Tweeter</h1>
 					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
 						<Image src="/images/Tweeter-Main.png" alt="portfolio" layout="fill" className="object-contain" />
 					</div>
