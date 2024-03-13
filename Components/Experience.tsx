4422f3b70e135f22ac0a061bfd2691d1a6921882
MikeTheFyke <mikefyke@hotmail.com>
2024-03-13 17:30:59 -0400

    Updating experience cards styles

--- Components/Experience.tsx
+++ Components/Experience.tsx
@@ -24,18 +24,22 @@ const Experience = () => {
 							<h1 className="text-[20px] md:text-[18px] mt-[1.5rem] mb-[1rem]">April 2021 - January 2024</h1>
 						</div>
 						<div className="md:grid-cols-2 lg:grid-cols-2 w-[80%] mx-auto items-center gap-[0.5rem]">
-							<ul className="text-[15px] text-[#e8e7e7] font-normal">
-								<li>Fixed/reported bugs</li>
-								<li>Built new features</li>
-								<li>Improved old features</li>
-								<li>Took part in code reviews</li>
-							</ul>
-							<ul className="text-[15px] text-[#e8e7e7] font-normal">
-								<li>Built an avatar upload feature</li>
-								<li>Built an internal integration portal</li>
-								<li>Managed a frontend team</li>
-								<li>Took part in integration meetings</li>
-							</ul>
+							<div>
+								<ul className="text-[15px] text-[#e8e7e7] font-normal">
+									<li>Fixed/reported bugs</li>
+									<li>Built new features</li>
+									<li>Improved old features</li>
+									<li>Took part in code reviews</li>
+								</ul>
+							</div>
+							<div>
+								<ul className="text-[15px] text-[#e8e7e7] font-normal">
+									<li>Built an avatar upload feature</li>
+									<li>Built an internal integration portal</li>
+									<li>Managed a frontend team</li>
+									<li>Took part in integration meetings</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>
