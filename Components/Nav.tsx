03fc5f045c20f7019bc36c2335e6b8169799f2d3
MikeTheFyke <mikefyke@hotmail.com>
2024-03-12 21:48:02 -0400

    Added spacing in nav

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -9,7 +9,7 @@ const Nav = ({ openNav }: Props) => {
 	return (
 		<div className="w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md">
 			<div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
-				<a href="#Hero">
+				<a href="#Hero" className="mr-[2rem]">
 					<h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
 						MIKE
 						<span className="text-red-500">FYKE</span>
