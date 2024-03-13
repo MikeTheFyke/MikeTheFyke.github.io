a3932e34e626dcd3885f737405c77fcfeecacda5
MikeTheFyke <mikefyke@hotmail.com>
2024-03-13 13:52:02 -0400

    gsap test

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -20,7 +20,7 @@ const Nav = ({ openNav }: Props) => {
 	return (
 		<div ref={navMain} className="w-[100%] fixed z-[10000] top-0 h-[12vh] bg-[#141c27] shadow-md">
 			<div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
-				<div ref={navFyke} className="hidden">
+				<div ref={navFyke} style={{ display: "hidden" }}>
 					<a href="#Hero" className="mr-[4rem]">
 						<h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">
 							MIKE
