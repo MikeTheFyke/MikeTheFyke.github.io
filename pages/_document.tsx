5e95d885bdf2fcf635b6240dbd140e668762fdd3
MikeTheFyke <mikefyke@hotmail.com>
2024-03-13 14:09:35 -0400

    Adjusting overflow-x

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html lang="en" className="overflow-x-hidden scroll-smooth bg-[#253348]">
+		<Html lang="en" className=" scroll-smooth bg-[#253348]">
 			<Head />
 			<title>Mike Fykes Portfolio</title>
 			<body className="font-primary">
