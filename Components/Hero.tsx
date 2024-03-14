8f0a68ac0b94b77ef90fae20adeae0d68c2ba8db
MikeTheFyke <mikefyke@hotmail.com>
2024-03-14 10:28:34 -0400

    New color Pallete for Hero

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
 							id="downloadButton"
 						>
 							<a href="/MikeFyke-Resume.pdf">Download CV</a>
-							<ArrowDownTrayIcon className="w-[1.6rem] h[1.7rem] text-black" />
+							<ArrowDownTrayIcon className="w-[1.6rem] h[1.7rem] text-black hover:text-white" />
 						</button>
 					</div>
 				</div>
