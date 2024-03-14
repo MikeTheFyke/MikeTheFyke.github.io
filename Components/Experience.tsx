12e3d791843b0b91ae20d0cef8be56e6786228be
MikeTheFyke <mikefyke@hotmail.com>
2024-03-14 09:06:46 -0400

    Stylizing experience glass card

--- Components/Experience.tsx
+++ Components/Experience.tsx
@@ -50,7 +50,42 @@ const Experience = () => {
 						</div>
 					</div>
 				</div>
-
+				{/* Glass 02 */}
+				<div
+					className="border border-blue-300 shadow rounded-md p-4 w-full mx-auto hover:scale-110 cursor-pointer mt-[2rem]"
+					onClick={() => openNewTab("https://www.123loadboard.com")}
+				>
+					<div className="animate-pulse flex space-x-4">
+						<Image src="/images/123Loadboard.png" alt="companyAvatar" className="w-[4rem] h-[4em] mx-auto" />
+						<div className="flex-1 space-y-6 py-1">
+							<div>
+								<h1 className="text-[20px] md:text-[30px] mt-[.25rem]  text-slate-700 font-bold">123Loadboard</h1>
+								<h1 className="text-[20px] md:text-[18px] text-slate-700 font-semibold">Frontend Developer</h1>
+								<h1 className="text-[20px] md:text-[18px] text-slate-700">April 2021 - January 2024</h1>
+							</div>
+							<div className="space-y-3">
+								<div className="grid grid-cols-2 gap-4">
+									<div className="">
+										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-item">
+											<li>Fixed/reported bugs.</li>
+											<li>Built new features.</li>
+											<li>Improved old features.</li>
+											<li>Took part in code reviews.</li>
+										</ul>
+									</div>
+									<div className="">
+										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-item">
+											<li>Built an avatar upload feature.</li>
+											<li>Built an internal integration portal.</li>
+											<li>Managed a frontend team.</li>
+											<li>Took part in integration meetings.</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				{/* <div data-aos="fade-left" data-aos-delay="300">
 					<div className="bg-gray-800 hover:scale-110 transform transition-all duration-300 hover:rotate-6 uppercase font-semibold p-[2rem] rounded">
 						<div className="text-center">
