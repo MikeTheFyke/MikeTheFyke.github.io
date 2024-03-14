110f550a211f050811e3e669a5b9ae9fa9467a06
MikeTheFyke <mikefyke@hotmail.com>
2024-03-14 17:21:49 -0400

    Updating color palette in about me

--- Components/About.tsx
+++ Components/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
 	};
 
 	return (
-		<div className="bg-[#121121] pb-[3rem] pt-[4rem] md:pt-[8rem]">
+		<div className="bg-[#2B445E] pb-[3rem] pt-[4rem] md:pt-[8rem]">
 			<div className="grid grid-cols-1 md:grid-cols-2 w-[80%] mx-auto gap-[3rem] items-center">
 				<div>
 					<h2 className="text-[25px] md:text-[35px] lg:text-[45px] md:leading-[3rem] leading-[2rem] capitalize mb-[3rem] font-bold text-white">
@@ -26,17 +26,17 @@ const About = () => {
 					</h2>
 					<div className="mb-[3rem] flex items-center md:space-x-10">
 						<div style={{ width: "50px", height: "50px" }} onMouseOver={() => onHover(plusSlate1)} onMouseLeave={() => onLeave(plusSlate1)}>
-							<span ref={plusSlate1} className="w-[50px] hidden md:block h-[5px] bg-slate-400 rounded-sm"></span>
+							<span ref={plusSlate1} className="w-[50px] hidden md:block h-[5px] text-[#EFEFEF] rounded-sm"></span>
 						</div>
-						<p className="text-[19px] text-slate-300 w-[80%]">
+						<p className="text-[19px] text-[#EFEFEF] w-[80%]">
 							Make users feel like they are experiencing a website instead of just viewing it, with micro-interactions.
 						</p>
 					</div>
 					<div className="mb-[3rem] flex items-center md:space-x-10">
 						<div style={{ width: "50px", height: "50px" }} onMouseOver={() => onHover(plusSlate2)} onMouseLeave={() => onLeave(plusSlate2)}>
-							<span ref={plusSlate2} className="w-[50px] hidden md:block h-[5px] bg-slate-400 rounded-sm"></span>
+							<span ref={plusSlate2} className="w-[50px] hidden md:block h-[5px] text-[#EFEFEF] rounded-sm"></span>
 						</div>
-						<p className="text-[19px] text-slate-300 w-[80%]">
+						<p className="text-[19px] text-[#EFEFEF] w-[80%]">
 							Guidlines can be like a fence around a yard. Some people can feel contained and stay firmly in the middle of the yard,
 							contained by those guidelines. But to truely make the best use of those guidlines somone needs to walk along that fence like a
 							tight rope.
@@ -50,13 +50,6 @@ const About = () => {
 						layout="fill"
 						objectFit="contain"
 						className="relative z-[11] w-[100%] h-[100%] object-contain"
-					/>
-					<Image
-						src="/images/MikeFyke-Gen02.jpeg"
-						alt="user"
-						layout="fill"
-						objectFit="contain"
-						className="absolute w-[100%] h-[100%] z-[10] bg-[#47B5E0] top-[-2rem] right-[-2rem]"
 					/>
 				</div>
 			</div>
