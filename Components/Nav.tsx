17eb8d969765d716b4ff0607cc0bd6f0e5c6cde3
MikeTheFyke <mikefyke@hotmail.com>
2024-03-14 20:39:05 -0400

    Consolidating nav styles

--- Components/Nav.tsx
+++ Components/Nav.tsx
@@ -52,14 +52,11 @@ const Nav = ({ openNav }: Props) => {
 	}, []);
 
 	return (
-		<div
-			ref={navMain}
-			className="w-[100%] fixed z-[10000] top-0 lg:h-[116px] md:h-[116px] bg-[#ABABAB] shadow-md"
-		>
-			<div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
+		<div ref={navMain} className="nav-main-container">
+			<div className="nav-inner-container ">
 				<div ref={navFyke} style={{ display: "hidden" }}>
 					<a href="#Hero" className="mr-[4rem]">
-						<h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+						<h1 className="text-white">
 							MIKE
 							<span className="text-red-500">FYKE</span>
 						</h1>
