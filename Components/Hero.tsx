0734128b9425209cfbf0f8f8587326bf99b21cee
MikeTheFyke <mikefyke@hotmail.com>
2024-03-15 16:47:38 -0400

    Updated timeline

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -15,9 +15,9 @@ const Hero = () => {
 		const tl = gsap.timeline();
 		tl.to(avatarCardImage1.current, 0.5, { rotationY: 90 });
 		tl.to(avatarCardImage1.current, 0.5, {
-			rotationY: 0,
 			backgroundImage:
 				currentAvatar === avatarImage2 ? avatarImage1 : avatarImage2,
+			rotationY: 0,
 			delay: 1,
 		});
 		nextImage();
