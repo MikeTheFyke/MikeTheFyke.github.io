df8f62abf4c6d38c743e3650783cc70bb4a37d88
MikeTheFyke <mikefyke@hotmail.com>
2024-03-15 17:33:47 -0400

    Updating color palette and ordering of sections

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -23,7 +23,7 @@ const Contact = () => {
 						<h1 className="text-[25px] mb-[0.2rem] font-semibold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 							Email
 						</h1>
-						<p className="text-[17px] w-[90%] text-white opacity-60 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+						<p className="text-[18px] w-[90%] text-white opacity-60 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 							mikefyke@hotmail.com
 						</p>
 					</div>
@@ -37,7 +37,7 @@ const Contact = () => {
 						<h1 className="text-[25px] mb-[0.2rem] font-semibold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 							Phone
 						</h1>
-						<p className="text-[17px] w-[90%] text-white opacity-60 whitespace-nowrap drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+						<p className="text-[18px] w-[90%] text-white opacity-60 whitespace-nowrap drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 							1-705-808-1229
 						</p>
 					</div>
@@ -51,42 +51,36 @@ const Contact = () => {
 						<h1 className="text-[25px] mb-[0.2rem] font-semibold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 							Address
 						</h1>
-						<p className="text-[17px] w-[90%] text-white opacity-60 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+						<p className="text-[18px] w-[90%] text-white opacity-60 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 							Toronto, Ontario, Canada
 						</p>
 					</div>
 				</div>
 			</div>
-			<div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 items-center justify-between">
-				<div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-20 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+			<div className="w-[80%] mt-[2rem] mx-auto flex items-center justify-between">
+				<div className="text-[18px] mb-[2rem] md:mb-0 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 					Mike Fyke 2024 | All Rights Reserved
 				</div>
-				<div className="flex items-center space-x-10">
-					<div onClick={() => openNewTab("htts://www.github.com/MikeTheFyke")}>
-						<Image
-							src={"/images/GithubAvatar.png"}
-							alt={"github"}
-							width={50}
-							height={50}
-							objectFit="contain"
-							className="mx-auto mb-[2rem] rounded-full bg-[#FFFFFF] cursor-pointer drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
-						/>
-					</div>
-					<div
-						onClick={() =>
-							openNewTab("https://www.linkedin.com/in/mike-fyke-5b4a5aa0/")
-						}
-					>
-						<Image
-							src={"/images/LinkedinAvatar.png"}
-							alt={"linkedin"}
-							width={50}
-							height={50}
-							objectFit="contain"
-							className="mx-auto mb-[2rem] rounded-full bg-[#FFFFFF] cursor-pointer drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
-						/>
-					</div>
-				</div>
+				<Image
+					onClick={() => openNewTab("htts://www.github.com/MikeTheFyke")}
+					src={"/images/GithubAvatar.png"}
+					alt={"github"}
+					width={50}
+					height={50}
+					objectFit="contain"
+					className="mx-auto mb-[2rem] rounded-full bg-[#FFFFFF] cursor-pointer drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+				/>
+				<Image
+					onClick={() =>
+						openNewTab("https://www.linkedin.com/in/mike-fyke-5b4a5aa0/")
+					}
+					src={"/images/LinkedinAvatar.png"}
+					alt={"linkedin"}
+					width={50}
+					height={50}
+					objectFit="contain"
+					className="mx-auto mb-[2rem] rounded-full bg-[#FFFFFF] cursor-pointer drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+				/>
 			</div>
 		</div>
 	);
