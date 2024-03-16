57da5116b6fc69a0733d86560b24e36dfc8f75b5
MikeTheFyke <mikefyke@hotmail.com>
2024-03-15 22:56:21 -0400

    Skills text styles

--- Components/Experience.tsx
+++ Components/Experience.tsx
@@ -77,7 +77,7 @@ const Experience = () => {
 						/>
 						<div className="flex-1 space-y-6 py-1">
 							<div>
-								<h1 className="text-[20px] md:text-[30px] mt-[.25rem]  text-slate-700 font-bold">
+								<h1 className="text-[20px] md:text-[30px] mt-[.25rem] text-slate-700 font-bold">
 									The Prosperous Farm
 								</h1>
 								<h1 className="text-[20px] md:text-[18px] text-slate-700 font-semibold">
@@ -90,7 +90,7 @@ const Experience = () => {
 							<div className="space-y-3">
 								<div className="grid grid-cols-2 gap-4">
 									<div className="">
-										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-disc">
+										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-disc drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 											<li>Fixed/reported bugs</li>
 											<li>Built new features</li>
 											<li>Improved old features</li>
