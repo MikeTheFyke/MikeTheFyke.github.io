a0f88151afd7a5e1e53eb3ef20d78c7a34b8c5cc
MikeTheFyke <mikefyke@hotmail.com>
2024-03-15 20:56:08 -0400

    Updating contact footer grid2

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -57,11 +57,11 @@ const Contact = () => {
 					</div>
 				</div>
 			</div>
-			<div className="w-[80%] mt-[2rem] mx-auto flex grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-between">
-				<div className="w-[80%] text-[18px] mb-[2rem] md:mb-0 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+			<div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-2 md:grid-cols-2 lg:grid-cols-2 justify-between">
+				<div className="w-[60%] text-[18px] mb-[2rem] md:mb-0 text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 					Mike Fyke 2024 | All Rights Reserved
 				</div>
-				<div className="w-[20%]">
+				<div className="w-[20%] block">
 					<Image
 						onClick={() => openNewTab("htts://www.github.com/MikeTheFyke")}
 						src={"/images/GithubAvatar.png"}
