398aed3647cf3154caf00d5b7462fc413ac8a48b
MikeTheFyke <mikefyke@hotmail.com>
2024-03-15 22:33:20 -0400

    Skills text styles

--- Components/SkillsItem.tsx
+++ Components/SkillsItem.tsx
@@ -9,7 +9,7 @@ interface Props {
 const SkillsItem = ({ title, year, description }: Props) => {
 	return (
 		<div className="mb-[2rem] md:mb-[2rem]">
-			<span className="px-[2rem] text-[#EFEFEF] py-[0.9rem] font-bold text-[18px] border-[2px] border-[#EFEFEF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+			<span className="px-[2rem] text-[#EFEFEF] py-[0.9rem] font-bold text-[18px] border-[2px] bg-[white] border-[#EFEFEF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 				{year}
 			</span>
 			<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-[#EFEFEF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
