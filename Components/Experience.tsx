1151baae1ccf7098202355eec623b07f1c84d189
MikeTheFyke <mikefyke@hotmail.com>
2024-03-15 22:39:38 -0400

    Skills text styles

--- Components/Experience.tsx
+++ Components/Experience.tsx
@@ -47,7 +47,7 @@ const Experience = () => {
 							<div className="space-y-3">
 								<div className="grid grid-cols-2 gap-4">
 									<div className="">
-										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-disc">
+										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-disc drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 											<li>Fixed/reported bugs.</li>
 											<li>Built new features.</li>
 											<li>Improved old features.</li>
@@ -55,7 +55,7 @@ const Experience = () => {
 										</ul>
 									</div>
 									<div className="">
-										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-disc">
+										<ul className="text-[15px] text-[#e8e7e7] font-normal pl-2 list-disc drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 											<li>Built an avatar upload feature.</li>
 											<li>Built an internal integration portal.</li>
 											<li>Managed a frontend team.</li>
