ba9d5705560da0940040f5d4496fc1827c58d4a3
MikeTheFyke <mikefyke@hotmail.com>
2024-03-18 16:27:44 -0400

    Consolidating project styles

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -13,28 +13,23 @@ const Projects = () => {
 
 			<div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-[2rem]">
 				<div>
-					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-						Better Bytes
-					</h1>
-					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
-						<div>
-							<Image
-								src="/images/BetterBytes-Main.jpeg"
-								alt="portfolio"
-								className="object-contain"
-								style={{ borderRadius: "50px" }}
-							/>
-						</div>
+					<h1 className="project-title">Better Bytes</h1>
+					<div className="project-image-container">
+						<Image
+							src="/images/BetterBytes-Main.jpeg"
+							alt="portfolio"
+							layout="fill"
+							className="object-contain"
+						/>
 					</div>
-					<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+					<h1 className="project-description">
 						NodeJs, ReactJs, Express, React-Router, Rellax, PostgresSQL, Knex,
 						Cheerio, Bootstrap
 					</h1>
 				</div>
+
 				<div>
-					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-						Owl Site
-					</h1>
+					<h1 className="project-title">Owl Site</h1>
 					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/OwlSite-Main.jpeg"
@@ -43,14 +38,10 @@ const Projects = () => {
 							className="object-contain"
 						/>
 					</div>
-					<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-white text-[20px] font-bold">
-						Vue, GSAP, Express, MongoDB
-					</h1>
+					<h1 className="project-description">Vue, GSAP, Express, MongoDB</h1>
 				</div>
 				<div>
-					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-						Shrink-A-Link
-					</h1>
+					<h1 className="project-title">Shrink-A-Link</h1>
 					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/ShrinkALink-Main.jpeg"
@@ -59,14 +50,12 @@ const Projects = () => {
 							className="object-contain"
 						/>
 					</div>
-					<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+					<h1 className="project-description">
 						NodeJs, Express, EJS, JQuery, Bootstrap
 					</h1>
 				</div>
 				<div>
-					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-						Xpedia
-					</h1>
+					<h1 className="project-title">Xpedia</h1>
 					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/Xpedia-Main.jpeg"
@@ -75,14 +64,10 @@ const Projects = () => {
 							className="object-contain"
 						/>
 					</div>
-					<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-white text-[20px] font-bold">
-						NodeJs, VueJs, GSAP
-					</h1>
+					<h1 className="project-description">NodeJs, VueJs, GSAP</h1>
 				</div>
 				<div>
-					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-						Chatter Box
-					</h1>
+					<h1 className="project-title">Chatter Box</h1>
 					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/ChatterBox-Main.jpeg"
@@ -91,14 +76,12 @@ const Projects = () => {
 							className="object-contain"
 						/>
 					</div>
-					<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+					<h1 className="project-description">
 						NodeJs, React, React-dom, Express, Webpack
 					</h1>
 				</div>
 				<div>
-					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-						Tweeter
-					</h1>
+					<h1 className="project-title">Tweeter</h1>
 					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/Tweeter-Main.jpeg"
@@ -107,7 +90,7 @@ const Projects = () => {
 							className="object-contain"
 						/>
 					</div>
-					<h1 className="p-5 w-[100%] bg-gray-800 rounded-sm text-white text-[20px] font-bold">
+					<h1 className="project-description">
 						HTML, CSS, JQuery, Express, MongoDB
 					</h1>
 				</div>
