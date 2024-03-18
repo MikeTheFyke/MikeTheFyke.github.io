66f8861ca2f014b163df862e52507a0b1e58a549
MikeTheFyke <mikefyke@hotmail.com>
2024-03-18 11:55:39 -0400

    Removed hover transform on projects

--- Components/Projects.tsx
+++ Components/Projects.tsx
@@ -11,11 +11,11 @@ const Projects = () => {
 				Pro<span className="text-red-500">jects</span>
 			</h1>
 			<div className="w-[80%] pt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
-				<div data-aos="fade-up" className="hover:-translate-y-6">
+				<div>
 					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 						Better Bytes
 					</h1>
-					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/BetterBytes-Main.jpeg"
 							alt="portfolio"
@@ -28,15 +28,11 @@ const Projects = () => {
 						Cheerio, Bootstrap
 					</h1>
 				</div>
-				<div
-					data-aos="fade-up"
-					data-aos-delay="300"
-					className="hover:-translate-y-6"
-				>
+				<div>
 					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 						Owl Site
 					</h1>
-					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/OwlSite-Main.png"
 							alt="portfolio"
@@ -48,15 +44,11 @@ const Projects = () => {
 						Vue, GSAP, Express, MongoDB
 					</h1>
 				</div>
-				<div
-					data-aos="fade-up"
-					data-aos-delay="600"
-					className="hover:-translate-y-6"
-				>
+				<div>
 					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 						Shrink-A-Link
 					</h1>
-					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/ShrinkALink-Main.png"
 							alt="portfolio"
@@ -68,15 +60,11 @@ const Projects = () => {
 						NodeJs, Express, EJS, JQuery, Bootstrap
 					</h1>
 				</div>
-				<div
-					data-aos="fade-up"
-					data-aos-delay="900"
-					className="hover:-translate-y-6"
-				>
+				<div>
 					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 						Chatter Box
 					</h1>
-					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/ChatterBox-Main.png"
 							alt="portfolio"
@@ -88,15 +76,11 @@ const Projects = () => {
 						NodeJs, React, React-dom, Express, Webpack
 					</h1>
 				</div>
-				<div
-					data-aos="fade-up"
-					data-aos-delay="1200"
-					className="hover:-translate-y-6"
-				>
+				<div>
 					<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-white text-center drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 						Tweeter
 					</h1>
-					<div className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
+					<div className="transform cursor-pointer relative w-[100%] h-[200px] md:h-[300px]">
 						<Image
 							src="/images/Tweeter-Main.png"
 							alt="portfolio"
