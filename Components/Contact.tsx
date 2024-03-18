7283a4a1f0167fa4d654470d85f5262788578e6b
MikeTheFyke <mikefyke@hotmail.com>
2024-03-18 11:42:14 -0400

    Added telephone link to phone icon

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -31,7 +31,9 @@ const Contact = () => {
 
 				<div className="flex items-center">
 					<div className="w-[5rem] h-[5rem] flex items-center justify-center rounded-full  bg-[#FFFFFF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-						<DevicePhoneMobileIcon className="w-[3rem] h-[3rem] text-black" />
+						<a href="tel:17058081229">
+							<DevicePhoneMobileIcon className="w-[3rem] h-[3rem] text-black" />
+						</a>
 					</div>
 					<div style={{ marginLeft: "10px" }}>
 						<h1 className="text-[25px] mb-[0.2rem] font-semibold text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
