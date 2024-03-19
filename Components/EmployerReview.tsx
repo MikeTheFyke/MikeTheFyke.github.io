9fe08a6464acf888fbd1395ff8ba80fb69e580c3
MikeTheFyke <mikefyke@hotmail.com>
2024-03-19 13:43:49 -0400

    Adding padding to the bottom of carousel elements

--- Components/EmployerReview.tsx
+++ Components/EmployerReview.tsx
@@ -11,11 +11,22 @@ interface Props {
 
 const EmployerReview = ({ image, name, role, review }: Props) => {
 	return (
-		<div className="flex flex-col text-center justify-center ">
-			<Image src={image} alt={name} width={100} height={100} objectFit="contain" className="mx-auto mb-[2rem] rounded-full" />
+		<div className="flex flex-col text-center justify-center pb-[20px] ">
+			<Image
+				src={image}
+				alt={name}
+				width={100}
+				height={100}
+				objectFit="contain"
+				className="mx-auto mb-[2rem] rounded-full"
+			/>
 			<h1 className="text-[25px] text-white mt-[1rem]">{name}</h1>
-			<p className="text-[18px] text-white opacity-75 mt-[0.5rem] mb-[1.4rem]">{role}</p>
-			<p className="text-[16px] text-white opacity-50 w-[90%] md:w-[50%] mx-auto">{review}</p>
+			<p className="text-[18px] text-white opacity-75 mt-[0.5rem] mb-[1.4rem]">
+				{role}
+			</p>
+			<p className="text-[16px] text-white opacity-50 w-[90%] md:w-[50%] mx-auto">
+				{review}
+			</p>
 		</div>
 	);
 };
