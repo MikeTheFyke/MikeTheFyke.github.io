83a22f09683a8dcc030ba5efa02476f0fcbd809b
MikeTheFyke <mikefyke@hotmail.com>
2024-03-21 09:59:36 -0400

    Updated contact container

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -14,7 +14,7 @@ const Contact = () => {
 
 	return (
 		<div className="pt-[4rem] pb-[4rem] bg-[#ABABAB]" id="Contact">
-			<div className="nav-main-container">
+			<div className="grid pb-[1rem] border-gray-400 grid-cols-3 lg:grid-cols-5 md:grid-cols-3 w-[90%] mx-auto gap-[3rem]">
 				<div className="cursor-pointer w-[5rem] h-[5rem] flex items-center justify-center rounded-full  bg-[#FFFFFF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 					<a href="mailto:mikefyke@hotmail.com?subject=Lets%20Talk">
 						<EnvelopeIcon className="w-[3rem] h-[3rem] text-black" />
