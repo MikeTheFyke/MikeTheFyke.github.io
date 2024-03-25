d4dad4ec7b930b55d69ac2b0d22cc0529e4bb897
MikeTheFyke <mikefyke@hotmail.com>
2024-03-25 16:56:09 -0400

    Updated project descriptions

--- public/data/data.tsx
+++ public/data/data.tsx
@@ -41,31 +41,35 @@ export const projects = [
 	{
 		name: "Owl Site",
 		image: "/images/OwlSite-Main.jpeg",
-		summary: "Summary",
+		summary:
+			"Owl Site is a freight assistant app. Helping shippers build purchase orders, item by item. You can add new carriers to a list of carriers in a database. A user can also keep track of previous shipments to give more power in future negotiations.",
 		stack: "Vue, GSAP, Express, MongoDB",
 	},
 	{
 		name: "Xpedia",
 		image: "/images/Xpedia-Main.jpeg",
-		summary: "Summary",
+		summary:
+			"Build a trip to anywhere among the stars you can think of. Grab a shuttle, book a rover for when you arrive and boldly go where noone has gone before.",
 		stack: "NodeJs, VueJs, GSAP",
 	},
 	{
 		name: "Shrink-A-Link",
 		image: "/images/ShrinkALink-Main.jpeg",
-		summary: "Summary",
+		summary:
+			"Shrinking urls much like TinURL. Login or register to start shrinking your links. Bored of shrinking links? A user can also upload an avatar to personalize their profile.",
 		stack: "NodeJs, Express, EJS, JQuery, Bootstrap",
 	},
 	{
 		name: "Chatter Box",
 		image: "/images/ChatterBox-Main.jpeg",
-		summary: "Summary",
+		summary:
+			"A client-side SPA that allows multiple users to chat with eachother. users area able to set their username and chat in real-time through the Websocket server.",
 		stack: "NodeJs, React, React-dom, Express, Webpack",
 	},
 	{
 		name: "Tweeter",
 		image: "/images/Tweeter-Main.jpeg",
-		summary: "Summary",
+		summary: "A Twitter, I mean X, SPA clone.",
 		stack: "HTML, CSS, JQuery, Express, MongoDB",
 	},
 ];
