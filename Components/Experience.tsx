7cec6fda8f00367c613d0f825d8590efa04430ee
MikeTheFyke <mikefyke@hotmail.com>
2024-03-25 16:22:11 -0400

    Consolidating static data

--- Components/Experience.tsx
+++ Components/Experience.tsx
@@ -1,52 +1,9 @@
-import {
-	CodeBracketSquareIcon,
-	CommandLineIcon,
-	RocketLaunchIcon,
-} from "@heroicons/react/20/solid";
 import React from "react";
 import Image from "next/image";
 import { Color } from "@/Color";
-
-interface Job {
-	icon: string;
-	company: string;
-	jobTitle: string;
-	length: string;
-	tasks: string[];
-}
+import { experience } from "@/public/data/data";
 
 const Experience = () => {
-	const experience = [
-		{
-			icon: "/images/123Loadboard.png",
-			company: "123Loadboard",
-			jobTitle: "Frontend Developer",
-			length: "April 2021 - January 2024",
-			link: "https://www.123loadboard.com",
-			tasks: [
-				"Fixed/reported bugs.",
-				"Built new features.",
-				"Improved old features.",
-				"Took part in code reviews.",
-				"Built an avatar upload feature.",
-				"Built an internal integration portal.",
-				"Managed a frontend team.",
-				"Took part in integration meetings.",
-			],
-		},
-		{
-			icon: "/images/ProsperousFarmAvatar.jpeg",
-			company: "The Prosperous Farm",
-			jobTitle: "Frontend (Intern)",
-			length: "February 2021 - April 2021",
-			tasks: [
-				"Fixed/reported bugs",
-				"Built new features",
-				"Improved old features",
-			],
-		},
-	];
-
 	const openNewTab = (url: string): void => {
 		const newWindow = window.open(url, "_blank", "noopener,noreferrer");
 		if (newWindow) newWindow.opener = null;
---
+++ public/data/data.tsx
@@ -0,0 +1,30 @@
+export const experience = [
+	{
+		icon: "/images/123Loadboard.png",
+		company: "123Loadboard",
+		jobTitle: "Frontend Developer",
+		length: "April 2021 - January 2024",
+		link: "https://www.123loadboard.com",
+		tasks: [
+			"Fixed/reported bugs.",
+			"Built new features.",
+			"Improved old features.",
+			"Took part in code reviews.",
+			"Built an avatar upload feature.",
+			"Built an internal integration portal.",
+			"Managed a frontend team.",
+			"Took part in integration meetings.",
+		],
+	},
+	{
+		icon: "/images/ProsperousFarmAvatar.jpeg",
+		company: "The Prosperous Farm",
+		jobTitle: "Frontend (Intern)",
+		length: "February 2021 - April 2021",
+		tasks: [
+			"Fixed/reported bugs",
+			"Built new features",
+			"Improved old features",
+		],
+	},
+];
