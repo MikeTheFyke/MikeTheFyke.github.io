57dc0a0023dca33c48ac994212c55cd7d373f540
MikeTheFyke <mikefyke@hotmail.com>
2024-03-26 15:37:09 -0400

    Updating education text styles

--- Components/SkillsItem.tsx
+++ Components/SkillsItem.tsx
@@ -1,3 +1,4 @@
+import { Color } from "@/Color";
 import React from "react";
 
 interface Props {
@@ -9,13 +10,23 @@ interface Props {
 const SkillsItem = ({ title, year, description }: Props) => {
 	return (
 		<div className="mb-[2rem] md:mb-[2rem]">
-			<span className="px-[2rem] text-[#ABABAB] py-[0.9rem] font-bold text-[18px] border-[2px] bg-[white] border-[#EFEFEF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+			<span
+				className="px-[2rem] py-[0.9rem] font-bold text-[18px] border-[2px] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+				style={{
+					borderColor: Color.BACKGROUND_BLUE,
+					backgroundColor: Color.BACKGROUND_WHITE,
+					color: Color.BACKGROUND_BLUE,
+				}}
+			>
 				{year}
 			</span>
 			<h1 className="mt-[2rem] uppercase font-semibold mb-[1rem] text-[20px] sm:text-[25px] md:text-[30px] text-[#EFEFEF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 				{title}
 			</h1>
-			<p className="text-[#ABABAB] font-normal w-[80%] text-[17px] opacity-80 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+			<p
+				className="font-normal w-[80%] text-[17px] opacity-80 drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]"
+				style={{ color: Color.BACKGROUND_BLUE }}
+			>
 				{description}
 			</p>
 		</div>
