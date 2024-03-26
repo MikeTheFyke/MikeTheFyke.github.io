52f0063bd15e311009facf072d885010c2d3d49e
MikeTheFyke <mikefyke@hotmail.com>
2024-03-26 10:17:43 -0400

    Adding onclick hero avatar to small 3

--- Components/Partials/HeroAvatarSm.tsx
+++ Components/Partials/HeroAvatarSm.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from "react";
-import Image from "next/image";
 import { gsap } from "gsap";
 
 const HeroAvatarSm = () => {
@@ -34,14 +33,15 @@ const HeroAvatarSm = () => {
 			<div
 				data-aos="zoom-in"
 				className="h-[250px] w-[250px] relative items-center rounded-full md:hidden lg:hidden"
-			>
-				<Image
-					src="/images/MikeFyke-Gen.jpeg"
-					alt="user"
-					layout="fill"
-					className="object-cover rounded-full"
-				/>
-			</div>
+				id={"card01"}
+				style={{
+					backgroundImage: currentAvatar,
+					backgroundRepeat: "no-repeat",
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+				ref={avatarCardImage1}
+			></div>
 		</div>
 	);
 };
