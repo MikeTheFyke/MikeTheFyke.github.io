e7fc4122b560e2085fc3390b1ec954113309b607
MikeTheFyke <mikefyke@hotmail.com>
2024-03-28 16:21:58 -0400

    Updating contact image size

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -71,7 +71,7 @@ const Contact = () => {
 					style={{
 						backgroundImage: `url("/images/BloggerAvatar.png")`,
 						backgroundRepeat: "no-repeat",
-						backgroundSize: "cover",
+						backgroundSize: "contain",
 						backgroundPosition: "center",
 					}}
 				/>
