d04e3b7620287e468689dab2044af5d2f0b08ce9
MikeTheFyke <mikefyke@hotmail.com>
2024-05-16 14:45:05 -0400

    Removed phone from contact component"

--- Components/Contact.tsx
+++ Components/Contact.tsx
@@ -1,8 +1,4 @@
-import {
-	DevicePhoneMobileIcon,
-	EnvelopeIcon,
-	MapIcon,
-} from "@heroicons/react/20/solid";
+import { EnvelopeIcon, MapIcon } from "@heroicons/react/20/solid";
 import React from "react";
 import { Color } from "@/Color";
 
@@ -22,12 +18,6 @@ const Contact = () => {
 				<div className="cursor-pointer w-[5rem] h-[5rem] flex items-center justify-center rounded-full  bg-[#FFFFFF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
 					<a href="mailto:mikefyke@hotmail.com?subject=Lets%20Talk">
 						<EnvelopeIcon className="w-[3rem] h-[3rem] text-black" />
-					</a>
-				</div>
-
-				<div className="cursor-pointer w-[5rem] h-[5rem] flex items-center justify-center rounded-full  bg-[#FFFFFF] drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
-					<a href="tel:17058081229">
-						<DevicePhoneMobileIcon className="w-[3rem] h-[3rem] text-black" />
 					</a>
 				</div>
 
